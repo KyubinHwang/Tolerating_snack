b3e962f1526f76048fc34395e099d49e01a67cb1
KyubinHwang <choco1drink@gmail.com>
2022-04-26 01:01:22 +0900

    파이어베이스 적용 후 데이터 유무 확인 후 페이지 이동 코드 작성

--- lib/root.dart
+++ lib/root.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
 import 'start.dart';
+import 'patience.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class RootPage extends StatelessWidget {
   const RootPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const StartPage();
+    CollectionReference users = FirebaseFirestore.instance.collection('info');
+    return FutureBuilder<DocumentSnapshot>(
+        future: users.doc('data').get(),
+        builder:
+            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (snapshot.hasData && !snapshot.data!.exists) {
+            return const StartPage();
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            Map<String, dynamic> data =
+                snapshot.data!.data() as Map<String, dynamic>;
+            return PatiencePage(
+                snack: data['snack'],
+                snackTimes: data['snackTimes'],
+                snackPrice: data['snackPrice'],
+                startDay: data['startDay']);
+          }
+
+          return const StartPage();
+        });
   }
 }
